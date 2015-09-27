ba1cb00285cc0e325855e4a7e4c9a0f3eb468d77
AlexKnauth <alexander@knauth.org>
2015-09-27 10:30:31 -0400

    start using lenses

--- client/structures.rkt
+++ client/structures.rkt
@@ -1,32 +1,35 @@
 #lang racket
-(require pict3d)
-(provide (struct-out movekey) (struct-out orb) (struct-out orbs) (struct-out game) (struct-out shot) (struct-out client) (struct-out message) (struct-out orbdefine)
-         (struct-out mypos) (struct-out mydir) (struct-out mycube) round-pos round-dir round-orbs-dir)
+(require pict3d lens unstable/lens/struct-provide)
+(provide (all-defined-out))
 
 ;; key and the speed it is moving in that direction, pos is the pos it had when it was pressed
-(struct movekey (key speed) #:prefab)
+(struct/lens movekey (key speed) #:prefab)
 ;corner 1 and 2 are pos for drawing and pos is where to move it
 ;time is time in milliseconds when it was shot
-(struct shot (corner1 corner2 pos yaw pitch time) #:prefab)
+(struct/lens shot (corner1 corner2 pos yaw pitch time) #:prefab)
 ;;position and time at last key change.list of moves. Pos is a list of 3 coordinates that is current. (continue on next line)
 ;;Dir is direction it is pointing and roll is how much the camera is rotated. mx and my are mouse coordinates
 ;;shots is a list of shots to draw and reload-time is time the player shot last
 ;;name and color are strings
-(struct orb (pos time movekeys dir roll shots reload-time name color hostname port) #:prefab)
+(struct/lens orb (pos time movekeys dir roll shots reload-time name color hostname port) #:prefab)
 ;;player is a orb and enemys is a list of orbs
-(struct orbs (player enemys) #:transparent)
+(struct/lens orbs (player enemys) #:transparent)
 ;orbs is an orbs and exit? is wheather or not to stop the state and close the window
 ;;mt is the time in milliseconds at last send of state
-(struct game (mode orbs kills deaths exit? mt) #:transparent)
+(struct/lens game (mode orbs kills deaths exit? mt) #:transparent)
 
+(define game-orbs-player-lens (lens-thrush game-orbs-lens orbs-player-lens))
+(define game-orbs-player-pos-lens (lens-thrush game-orbs-player-lens orb-pos-lens))
+(define game-orbs-player-time-lens (lens-thrush game-orbs-player-lens orb-time-lens))
+(define game-orbs-player-shots-lens (lens-thrush game-orbs-player-lens orb-shots-lens))
 
-(struct client (hostname port) #:prefab)
-(struct message (name data) #:prefab)
-(struct orbdefine (name color hostname port) #:prefab)
+(struct/lens client (hostname port) #:prefab)
+(struct/lens message (name data) #:prefab)
+(struct/lens orbdefine (name color hostname port) #:prefab)
 
-(struct mypos (x y z) #:prefab)
-(struct mydir (dx dy dz) #:prefab)
-(struct mycube (pos scale color) #:prefab)
+(struct/lens mypos (x y z) #:prefab)
+(struct/lens mydir (dx dy dz) #:prefab)
+(struct/lens mycube (pos scale color) #:prefab)
 
 (define (round-pos p)
   (pos
